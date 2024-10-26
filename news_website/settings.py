009a6273d63a9efa4421368e44f4596a68d5ba11
Darya <dryagaroosi@gmail.com>
2024-10-26 09:57:34 +0330

    bearer

--- news_website/settings.py
+++ news_website/settings.py
@@ -151,6 +151,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
--- news_website/urls.py
+++ news_website/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('news.urls')),  # اتصال به urls اپلیکیشن اصلی
+    path('', include('news.urls')),
 ]
