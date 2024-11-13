4ef2fff09a9435af07dfa0f4d29ca316ea588aed
Darya <dryagaroosi@gmail.com>
2024-11-13 11:38:43 +0330

    edit permission

--- news/urls.py
+++ news/urls.py
@@ -44,7 +44,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(AllowAny,), # استفاده از tuple
+    permission_classes=(AllowAny,),  # استفاده از tuple به جای list یا str
 )
 
 
