e75e63d7fde76ce2abdcb655606929b0186d483c
Darya <dryagaroosi@gmail.com>
2024-11-19 21:08:08 +0330

    Fixing_domain

--- news_website/settings.py
+++ news_website/settings.py
@@ -49,10 +49,35 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080", 
     "https://yourdomain.com",
     "http://localhost:8080",
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 
