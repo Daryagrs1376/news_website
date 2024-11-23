328faf33e47e982311ebd0161391e0e963707780
Darya <dryagaroosi@gmail.com>
2024-11-23 14:24:26 +0330

    ADD

--- news/management/commands/generate_fake_data.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    help = 'Generate fake data for the database'
-
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Generating fake data...")
-        # کد تولید داده‌های جعلی
-        self.stdout.write(self.style.SUCCESS("Fake data generation completed!"))
